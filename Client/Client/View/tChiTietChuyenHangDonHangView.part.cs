94f13092cb7476fe4bc7d848d1c2f1d47b7c5a5b
kudo183@gmail.com <kudo183@gmail.com>
2017-06-15 12:28:24 +0700

    - set tChiTietDonHang foreignkey picker filter base on MaDonHang of tChuyenHangDonHangDto.

---
+++ Client/Client/View/tChiTietChuyenHangDonHangView.part.cs
@@ -0,0 +1,62 @@
+﻿using Client.ViewModel;
+using huypq.SmtWpfClient.Abstraction;
+using Shared;
+using SimpleDataGrid;
+
+namespace Client.View
+{
+    public partial class tChiTietChuyenHangDonHangView : BaseView<tChiTietChuyenHangDonHangDto>
+    {
+        tChiTietChuyenHangDonHangDto selectedDto;
+
+        partial void InitUIPartial()
+        {
+            GridView.dataGrid.SelectionChanged += DataGrid_SelectionChanged;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (sender != e.OriginalSource)
+            {
+                return;
+            }
+
+            selectedDto = ViewModel.GetSelectedItem() as tChiTietChuyenHangDonHangDto;
+            if (selectedDto == null)
+            {
+                return;
+            }
+
+            if (selectedDto.MaChuyenHangDonHangtChuyenHangDonHangDto == null)
+            {
+                return;
+            }
+
+            Logger.Instance.Info("selected tChiTietChuyenHangDonHangDto changed: UpdateDonHangForeignKeyPicker", Logger.Categories.UI);
+            UpdateChiTietDonHangForeignKeyPicker();
+
+            selectedDto.PropertyChanged -= SelectedDto_PropertyChanged;
+            selectedDto.PropertyChanged += SelectedDto_PropertyChanged;
+        }
+        
+        private void SelectedDto_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(tChiTietChuyenHangDonHangDto.MaChuyenHangDonHang))
+            {
+                Logger.Instance.Info("MaChuyenHangDonHang changed: UpdateDonHangForeignKeyPicker", Logger.Categories.UI);
+                UpdateChiTietDonHangForeignKeyPicker();
+            }
+        }
+
+        private void UpdateChiTietDonHangForeignKeyPicker()
+        {
+            var vm = (GridView.dataGrid.Columns[2] as DataGridForeignKeyColumn).PopupView.DataContext as tChiTietDonHangViewModel;
+            var maDonHang = selectedDto.MaChuyenHangDonHangtChuyenHangDonHangDto.MaDonHang;
+            vm.HeaderFilters[1].DisableChangedAction(p => { p.IsUsed = true; p.FilterValue = maDonHang; });
+            if (_isLoaded == true)
+            {
+                vm.Load();
+            }
+        }
+    }
+}
