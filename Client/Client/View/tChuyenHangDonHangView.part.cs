d27f5ca2e8df2e853ea6a7f3e1610395f378b194
kudo183@gmail.com <kudo183@gmail.com>
2017-06-20 18:48:51 +0700

    - filter donhang xong=false

---
+++ Client/Client/View/tChuyenHangDonHangView.part.cs
@@ -0,0 +1,23 @@
+﻿using huypq.SmtWpfClient.Abstraction;
+using Shared;
+using SimpleDataGrid;
+
+namespace Client.View
+{
+    public partial class tChuyenHangDonHangView : BaseView<tChuyenHangDonHangDto>
+    {
+        partial void InitUIPartial()
+        {
+            var column = GridView.FindColumn(nameof(tChuyenHangDonHangDto.MaDonHang));
+            var donhangFKP = (column as DataGridForeignKeyColumn).PopupView as tDonHangView;
+
+            var filter = donhangFKP.GridView.FindHeaderFilter(nameof(tDonHangDto.Xong));
+            filter.DisableChangedAction(p => { p.IsUsed = true; p.FilterValue = false; p.IsHitTestVisible = false; });
+
+            if (donhangFKP.IsLoaded == true)
+            {
+                donhangFKP.ViewModel.Load();
+            }
+        }
+    }
+}
