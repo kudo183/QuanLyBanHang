c44158ba1576fe361759fff67102f159299f12e9
kudo183@gmail.com <kudo183@gmail.com>
2017-06-21 09:52:27 +0700

    - auto add tChiTietChuyenHangDonHang for tChiTietDonHang xong=false.

---
+++ Client/Client/View/ChuyenHangComplexView.part.cs
@@ -0,0 +1,59 @@
+﻿using Client.ViewModel;
+using huypq.SmtWpfClient.Abstraction;
+using QueryBuilder;
+using Shared;
+using System.Linq;
+
+namespace Client.View
+{
+    public partial class ChuyenHangComplexView : BaseComplexView
+    {
+        protected override void OnMoveFocus(IBaseView currentView, IBaseView nextView)
+        {
+            if (currentView is tChuyenHangDonHangView)
+            {
+                System.Windows.Input.Keyboard.Focus(nextView.GridView.dataGrid);
+                return;
+            }
+
+            base.OnMoveFocus(currentView, nextView);
+        }
+
+        protected override void OnAfterSave(IBaseView previousView, IBaseView currentView, IBaseView nextView)
+        {
+            base.OnAfterSave(previousView, currentView, nextView);
+
+            if (currentView is tChuyenHangDonHangView)
+            {
+                var view = nextView as tChiTietChuyenHangDonHangView;
+                var viewModel = view.DataContext as tChiTietChuyenHangDonHangViewModel;
+                var chdh = viewModel.ParentItem as tChuyenHangDonHangDto;
+                if (chdh != null && viewModel.Entities.Count == 0)
+                {
+                    var qe = new QueryExpression();
+                    qe.AddWhereOption<WhereExpression.WhereOptionInt, int>(
+                        WhereExpression.Equal, nameof(tChiTietDonHangDto.MaDonHang), chdh.MaDonHang);
+                    qe.AddWhereOption<WhereExpression.WhereOptionBool, bool>(
+                          WhereExpression.Equal, nameof(tChiTietDonHangDto.Xong), false);
+                    var chiTietDonHangsChuaXong = viewModel.DataService.Get<tChiTietDonHangDto>(qe).Items;
+
+                    var ctchdh = viewModel.DataService.GetByListInt<tChiTietChuyenHangDonHangDto>(
+                        nameof(tChiTietChuyenHangDonHangDto.MaChiTietDonHang), chiTietDonHangsChuaXong.Select(p => p.Ma).ToList());
+
+                    foreach (var ctdh in chiTietDonHangsChuaXong)
+                    {
+                        var soLuongConLai = ctdh.SoLuong - ctchdh.Where(p => p.MaChiTietDonHang == ctdh.Ma).Sum(p => p.SoLuong);
+                        var ct = new tChiTietChuyenHangDonHangDto
+                        {
+                            MaChiTietDonHang = ctdh.Ma,
+                            MaChuyenHangDonHang = chdh.Ma,
+                            SoLuong = soLuongConLai
+                        };
+
+                        viewModel.Entities.Add(ct);
+                    }
+                }
+            }
+        }
+    }
+}
