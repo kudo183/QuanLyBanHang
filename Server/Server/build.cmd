608f8a9a646ab8d3e6c7dda32322d799f76081be
kudo183@gmail.com <kudo183@gmail.com>
2017-07-24 23:57:53 +0700

    - add help comment.

--- Server/Server/build.cmd
+++ Server/Server/build.cmd
@@ -1,10 +1,13 @@
 rem install dotnet
 rem install npm
 rem install gulp:    npm install --global gulp-cli
+rem create IIS Application named test (No Managed Code, Identity is current loggin window account for sql access)
+rem mapping IIS site "Default Web Site" to publish output folder.
 
 %systemroot%\system32\inetsrv\appcmd stop apppool test
 %systemroot%\system32\inetsrv\appcmd stop site "Default Web Site"
 
+rem if only run gulp #publish, the command will break (not continue run dotnet build ...)
 cmd /c gulp #publish
 
 dotnet build -c Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
