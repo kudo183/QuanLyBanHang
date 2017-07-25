5c6c8604472fa72be430f7ef2544c3389bf53768
kudo183@gmail.com <kudo183@gmail.com>
2017-07-25 10:01:47 +0700

    - add default settings.json and build.cmd for client.

---
+++ Client/build.cmd
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /t:Build /p:Configuration=Release
+
+copy /B /Y Client\bin\Release\client.exe Release\client.exe
