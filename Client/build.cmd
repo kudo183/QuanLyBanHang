71c810fa18d395a516757bbda3fbc9dc7b3f0938
kudo183@gmail.com <kudo183@gmail.com>
2017-07-27 08:39:13 +0700

    - add /m for parallel build.

--- Client/build.cmd
+++ Client/build.cmd
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /t:Build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /t:Build /m /p:Configuration=Release
 
 copy /B /Y Client\bin\Release\client.exe Release\client.exe
