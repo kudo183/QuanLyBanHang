b4597397650edd4e91f306e25fc5a54a720a5911
kudo183 <kudo183@gmail.com>
2017-08-22 19:06:40 +0700

    - add TongSoTien Msg in tChiPhiView.

---
+++ Client/Client/ViewModel/tChiPhiViewModel.part.cs
@@ -0,0 +1,14 @@
+﻿using huypq.SmtWpfClient.Abstraction;
+using Shared;
+using System.Linq;
+
+namespace Client.ViewModel
+{
+    public partial class tChiPhiViewModel : BaseViewModel<tChiPhiDto>
+    {
+        protected override void AfterLoad()
+        {
+            Msg = string.Format("Tong so tien: {0:N0}", Entities.Sum(p => p.SoTien));
+        }
+    }
+}
