e3d6dfc1dc5b2775d7f6f3609f700b6e5bce4c9f
gaucon <kudo183@gmail.com>
2017-09-16 18:54:38 +0700

    - add SmtFile.sql, column MaHinhAnh in tMatHang.sql

---
+++ Server/Database/dbo/Tables/SmtFile.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SmtFile] (
+    [ID]             INT            IDENTITY (1, 1) NOT NULL,
+    [TenantID]       INT            NOT NULL,
+    [FileName]       NVARCHAR (256) NOT NULL,
+    [FileSize]       BIGINT         NOT NULL,
+    [MimeType]       VARCHAR (128)  NOT NULL,
+    [CreateTime]     BIGINT         NOT NULL,
+    [LastUpdateTime] BIGINT         NOT NULL,
+    CONSTRAINT [PK_SmtFile] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
--- Server/Database/dbo/Tables/tMatHang.sql
+++ Server/Database/dbo/Tables/tMatHang.sql
@@ -6,10 +6,13 @@
     [SoMet]           INT            CONSTRAINT [DF_tMatHang_SoMet] DEFAULT ((0)) NOT NULL,
     [TenMatHangDayDu] NVARCHAR (200) CONSTRAINT [DF_tMatHang_TenMatHangDayDu] DEFAULT ('') NOT NULL,
     [TenMatHangIn]    NVARCHAR (50)  CONSTRAINT [DF_tMatHang_TenMatHangIn] DEFAULT ('') NOT NULL,
-    [TenantID]        INT            DEFAULT ((1)) NOT NULL,
-    [CreateTime]      BIGINT         DEFAULT ((0)) NOT NULL,
-    [LastUpdateTime]  BIGINT         DEFAULT ((0)) NOT NULL,
+    [MaHinhAnh]       INT            CONSTRAINT [DF_tMatHang_MaHinhAnh] DEFAULT ((0)) NOT NULL,
+    [TenantID]        INT            CONSTRAINT [DF__tMatHang__Tenant__019E3B86] DEFAULT ((1)) NOT NULL,
+    [CreateTime]      BIGINT         CONSTRAINT [DF__tMatHang__Create__23F3538A] DEFAULT ((0)) NOT NULL,
+    [LastUpdateTime]  BIGINT         CONSTRAINT [DF__tMatHang__LastUp__46486B8E] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Ma] ASC),
     CONSTRAINT [FK_tMatHang_rLoaiHang] FOREIGN KEY ([MaLoai]) REFERENCES [dbo].[rLoaiHang] ([Ma])
 );
+
+
 
