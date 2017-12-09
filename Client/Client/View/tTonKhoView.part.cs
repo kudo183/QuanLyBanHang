65acc03ee22259950ff399838f09400b4b671e8d
gaucon <kudo183@gmail.com>
2017-12-09 18:01:42 +0700

    - add Report Hang Nha La, Hang Nhap in tTonKhoView.

---
+++ Client/Client/View/tTonKhoView.part.cs
@@ -0,0 +1,41 @@
+﻿using Client.ViewModel;
+using System.Windows.Controls;
+
+namespace Client.View
+{
+    public partial class tTonKhoView
+    {
+        partial void InitUIPartial()
+        {
+            var btnTonHangNhap = new Button()
+            {
+                Content = "Copy Ton Hang Nhap",
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                Margin = new System.Windows.Thickness(5)
+            };
+            btnTonHangNhap.Click += BtnTonHangNhap_Click;
+            GridView.CustomMenuItems.Add(btnTonHangNhap);
+
+            var btnTonHangNha = new Button()
+            {
+                Content = "Copy Ton Hang Nha",
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                Margin = new System.Windows.Thickness(5)
+            };
+            btnTonHangNha.Click += BtnTonHangNha_Click;
+            GridView.CustomMenuItems.Add(btnTonHangNha);
+        }
+
+        private void BtnTonHangNhap_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = ViewModel as tTonKhoViewModel;
+            vm.CopyTonKhoToClipboard(false);
+        }
+
+        private void BtnTonHangNha_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = ViewModel as tTonKhoViewModel;
+            vm.CopyTonKhoToClipboard(true);
+        }
+    }
+}
