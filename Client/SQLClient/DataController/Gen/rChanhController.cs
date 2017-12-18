8bcc96930ab2602884633193dfac7adbef69bfae
gaucon <kudo183@gmail.com>
2017-12-18 14:49:24 +0700

    - add SQLClient.csproj

---
+++ Client/SQLClient/DataController/Gen/rChanhController.cs
@@ -0,0 +1,47 @@
+﻿using Shared;
+using huypq.SmtWpfClientSQL;
+using SQLClient.Entity;
+
+namespace SQLClient.DataController
+{
+    public partial class rChanhController : BaseDataController<SqlDbContext, rChanh, rChanhDto>
+    {
+        partial void ConvertToDtoPartial(ref rChanhDto dto, rChanh entity);
+        partial void ConvertToEntityPartial(ref rChanh entity, rChanhDto dto);
+
+        public override rChanhDto ConvertToDto(rChanh entity)
+        {
+            var dto = new rChanhDto()
+            {
+                ID = entity.ID,
+                MaBaiXe = entity.MaBaiXe,
+                TenChanh = entity.TenChanh,
+                TenantID = entity.TenantID,
+                CreateTime = entity.CreateTime,
+                LastUpdateTime = entity.LastUpdateTime
+            };
+
+            ConvertToDtoPartial(ref dto, entity);
+
+            return dto;
+        }
+
+        public override rChanh ConvertToEntity(rChanhDto dto)
+        {
+            var entity = new rChanh()
+            {
+                ID = dto.ID,
+                MaBaiXe = dto.MaBaiXe,
+                TenChanh = dto.TenChanh,
+                TenantID = dto.TenantID,
+                CreateTime = dto.CreateTime,
+                LastUpdateTime = dto.LastUpdateTime
+            };
+
+            ConvertToEntityPartial(ref entity, dto);
+
+            return entity;
+        }
+
+    }
+}
---
+++ Client/SQLClient/DataController/Gen/rKhoHangController.cs
@@ -0,0 +1,47 @@
+﻿using Shared;
+using huypq.SmtWpfClientSQL;
+using SQLClient.Entity;
+
+namespace SQLClient.DataController
+{
+    public partial class rKhoHangController : BaseDataController<SqlDbContext, rKhoHang, rKhoHangDto>
+    {
+        partial void ConvertToDtoPartial(ref rKhoHangDto dto, rKhoHang entity);
+        partial void ConvertToEntityPartial(ref rKhoHang entity, rKhoHangDto dto);
+
+        public override rKhoHangDto ConvertToDto(rKhoHang entity)
+        {
+            var dto = new rKhoHangDto()
+            {
+                ID = entity.ID,
+                TenKho = entity.TenKho,
+                TrangThai = entity.TrangThai,
+                TenantID = entity.TenantID,
+                CreateTime = entity.CreateTime,
+                LastUpdateTime = entity.LastUpdateTime
+            };
+
+            ConvertToDtoPartial(ref dto, entity);
+
+            return dto;
+        }
+
+        public override rKhoHang ConvertToEntity(rKhoHangDto dto)
+        {
+            var entity = new rKhoHang()
+            {
+                ID = dto.ID,
+                TenKho = dto.TenKho,
+                TrangThai = dto.TrangThai,
+                TenantID = dto.TenantID,
+                CreateTime = dto.CreateTime,
+                LastUpdateTime = dto.LastUpdateTime
+            };
+
+            ConvertToEntityPartial(ref entity, dto);
+
+            return entity;
+        }
+
+    }
+}
