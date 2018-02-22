57b2495db7e13e4a380c4f768aa32d1d2f56cd4f
gaucon <kudo183@gmail.com>
2018-02-22 17:42:54 +0700

    - auto add tChiTietChuyenHangDonHang for tChiTietDonHang xong=false of tDonHang in tChuyenHangDonHang.

--- WebClient/angular/src/app/chuyen-hang-complex/chuyen-hang-complex.component.ts
+++ WebClient/angular/src/app/chuyen-hang-complex/chuyen-hang-complex.component.ts
@@ -3,6 +3,9 @@ import { ChuyenHangComponent } from '../chuyen-hang/chuyen-hang.component';
 import { ChuyenHangDonHangComponent } from '../chuyen-hang-don-hang/chuyen-hang-don-hang.component';
 import { ChiTietChuyenHangDonHangComponent } from '../chi-tiet-chuyen-hang-don-hang/chi-tiet-chuyen-hang-don-hang.component';
 
+import { DataService, QueryExpression, WhereOption, WhereOptionTypes, OrderOption } from '../data.service';
+import { ReferenceDataService } from '../reference-data.service';
+
 @Component({
   selector: 'app-chuyen-hang-complex',
   templateUrl: './chuyen-hang-complex.component.html',
@@ -14,9 +17,63 @@ export class ChuyenHangComplexComponent implements AfterViewInit {
   @ViewChild('chuyenHangDonHangComp') chuyenHangDonHangComp: ChuyenHangDonHangComponent;
   @ViewChild('chiTietChuyenHangDonHangComp') chiTietChuyenHangDonHangComp: ChiTietChuyenHangDonHangComponent;
 
-  constructor() { }
+  constructor(private dataService: DataService, private refDataService: ReferenceDataService) { }
 
   ngAfterViewInit() {
+    this.chiTietChuyenHangDonHangComp.evAfterLoad.subscribe(p => {
+      const comp = this.chiTietChuyenHangDonHangComp;
+      if (comp.entities.length === 0) {
+        const maChuyenHangDonHang = comp.grid.settings.columnSettings[1].headerSetting.filterValue;
+        this.dataService.getByID('tChuyenHangDonHang', maChuyenHangDonHang).subscribe(chdh => {
+          const qe = new QueryExpression();
+          qe.addWhereOption('=', 'maDonHang', chdh.maDonHang, WhereOptionTypes.Int);
+          qe.addWhereOption('=', 'xong', false, WhereOptionTypes.Bool);
+          this.dataService.get('tChiTietDonHang', qe).subscribe(ctdhs => {
+            if (ctdhs.items.length > 0) {
+              this.refDataService.get('rkhohang').subscribe(khoHangs => {
+                this.refDataService.get('rkhachhang').subscribe(khachHangs => {
+                  this.refDataService.get('tmathang').subscribe(matHangs => {
+                    this.refDataService.get('rnhanvien').subscribe(nhanViens => {
+                      this.dataService.getByID('tDonHang', chdh.maDonHang).subscribe(dh => {
+                        this.dataService.getByID('tChuyenHang', chdh.maChuyenHang).subscribe(ch => {
+                          this.dataService.getIntList('tChiTietChuyenHangDonHang', 'maChiTietDonHang', ctdhs.items.map(p => p.id)).subscribe(ctchdhs => {
+                            const items = [];
+                            ctdhs.items.forEach(ctdh => {
+                              const newItem: any = {};
+                              newItem.maChuyenHangDonHang = maChuyenHangDonHang;
+                              newItem.chuyenHangDonHang = chdh;
+                              newItem.chuyenHangDonHang.chuyenHang = ch;
+                              newItem.chuyenHangDonHang.displayText = comp.getChuyenHangDonHangDisplayText(newItem.chuyenHangDonHang, nhanViens);
+
+                              newItem.maChiTietDonHang = ctdh.id;
+                              newItem.chiTietDonHang = ctdh;
+                              newItem.chiTietDonHang.donHang = dh;
+                              newItem.chiTietDonHang.displayText = comp.getChiTietDonHangDisplayText(newItem.chiTietDonHang, khoHangs, khachHangs, matHangs);
+
+                              let soLuongDaGiao = 0;
+                              ctchdhs.items.forEach(ctchdh => {
+                                if (ctchdh.maChiTietDonHang === newItem.maChiTietDonHang) {
+                                  soLuongDaGiao = soLuongDaGiao + ctchdh.soLuong;
+                                }
+                              });
+                              newItem.soLuong = ctdh.soLuong - soLuongDaGiao;
+
+                              items.push(newItem);
+                            });
+                            comp.entities = items;
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            }
+          });
+        });
+      }
+    });
+
     this.chuyenHangComp.grid.evSelectedItemChanged.subscribe(event => {
       this.chuyenHangDonHangComp.grid.settings.columnSettings[1].headerSetting.isEnableFilter = true;
       this.chuyenHangDonHangComp.grid.settings.columnSettings[1].headerSetting.filterValue = event.id;
