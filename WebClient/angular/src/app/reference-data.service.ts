736b016029fbc97f3f9f6aad4b6dfce09eb4a0b4
gaucon <kudo183@gmail.com>
2018-02-23 17:04:16 +0700

    - add isLoading to mark this data is waiting response from server. Other caller will notified after load finish by loadSubject.

--- WebClient/angular/src/app/reference-data.service.ts
+++ WebClient/angular/src/app/reference-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { DataService } from './data.service';
@@ -9,86 +10,58 @@ import { DataService } from './data.service';
 @Injectable()
 export class ReferenceDataService {
   private cache = {};
+
   constructor(private dataService: DataService) {
     const keyProperty = 'id';
-    this.cache['rloaihang'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenLoai',
-      isAscending: true
-    };
 
-    this.cache['tmathang'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenMatHang',
-      isAscending: true
-    };
+    this.cache['rloaihang'] = this.createCacheObject(keyProperty, 'tenLoai', true);
+    this.cache['tmathang'] = this.createCacheObject(keyProperty, 'tenMatHang', true);
+    this.cache['rkhohang'] = this.createCacheObject(keyProperty, 'tenKho', true);
+    this.cache['rkhachhang'] = this.createCacheObject(keyProperty, 'tenKhachHang', true);
+    this.cache['rnhanvien'] = this.createCacheObject(keyProperty, 'tenNhanVien', true);
+    this.cache['rloaichiphi'] = this.createCacheObject(keyProperty, 'tenLoaiChiPhi', true);
+    this.cache['rkhachhangchanh'] = this.createCacheObject(keyProperty, undefined, true);
+    this.cache['rchanh'] = this.createCacheObject(keyProperty, 'tenChanh', true);
+    this.cache['rcanhbaotonkho'] = this.createCacheObject(keyProperty, undefined, true);    
+  }
 
-    this.cache['rkhohang'] = {
+  private createCacheObject(keyProperty, sortProperty, isAscending) {
+    return {
       data: [],
       isLoaded: false,
       keyProperty: keyProperty,
-      sortProperty: 'tenKho',
-      isAscending: true
-    };
-
-    this.cache['rkhachhang'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenKhachHang',
-      isAscending: true
-    };
-
-    this.cache['rnhanvien'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenNhanVien',
-      isAscending: true
-    };
-
-    this.cache['rloaichiphi'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenLoaiChiPhi',
-      isAscending: true
-    };
-
-    this.cache['rkhachhangchanh'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: undefined,
-      isAscending: true
-    };
-
-    this.cache['rchanh'] = {
-      data: [],
-      isLoaded: false,
-      keyProperty: keyProperty,
-      sortProperty: 'tenChanh',
-      isAscending: true
-    };
+      sortProperty: sortProperty,
+      isAscending: isAscending,
+      isLoading: false,
+      loadSubject: new Subject()
+    }
   }
 
   get(controller: string) {
+    controller = controller.toLowerCase();
     const cache = this.cache[controller];
     if (cache.isLoaded === false) {
-      return this.dataService.getAll(controller).pipe(
-        tap(data => {
-          cache.isLoaded = true;
-          cache.data = data;
-          if (cache.sortProperty !== undefined) {
-            this.sort(cache.data.items, cache.sortProperty, cache.isAscending);
-          }
-        }));
+      if (cache.isLoading === false) {
+        cache.isLoading = true;
+        return this.dataService.getAll(controller).pipe(
+          tap(data => {
+            cache.isLoaded = true;
+            cache.data = data;
+            if (cache.sortProperty !== undefined) {
+              this.sort(cache.data.items, cache.sortProperty, cache.isAscending);
+            }
+            cache.isLoading = false;
+            cache.loadSubject.next(cache.data);
+          }));
+      } else {
+        return cache.loadSubject;
+      }
     }
     return of(cache.data);
+  }
+
+  gets(controllers: Array<string>): Observable<any> {
+    return Observable.forkJoin(controllers.map(p => this.get(p)));
   }
 
   getOrUpdate(controller: string) {
