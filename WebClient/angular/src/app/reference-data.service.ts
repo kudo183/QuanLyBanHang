648aaeab87799cc369ad0d17aca2d91ed51bdbad
gaucon <kudo183@gmail.com>
2018-02-27 12:09:19 +0700

    - update reference-data.

--- WebClient/angular/src/app/reference-data.service.ts
+++ WebClient/angular/src/app/reference-data.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs/observable/of';
-import { catchError, map, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { DataService } from './data.service';
 
 @Injectable()
@@ -12,29 +11,36 @@ export class ReferenceDataService {
   private cache = {};
 
   keyProperty = 'id';
+  sortProperty = 'displayText';
 
   constructor(private dataService: DataService) {
-    this.cache['rloaihang'] = this.createCacheObject('tenLoai', true, item => { return item.tenLoai; });
-    this.cache['tmathang'] = this.createCacheObject('tenMatHang', true, item => { return item.tenMatHang; });
-    this.cache['rkhohang'] = this.createCacheObject('tenKho', true, item => { return item.tenKho; });
-    this.cache['rkhachhang'] = this.createCacheObject('tenKhachHang', true, item => { return item.tenKhachHang; });
-    this.cache['rnhanvien'] = this.createCacheObject('tenNhanVien', true, item => { return item.tenNhanVien; });
-    this.cache['rloaichiphi'] = this.createCacheObject('tenLoaiChiPhi', true, item => { return item.tenLoaiChiPhi; });
-    this.cache['rkhachhangchanh'] = this.createCacheObject(undefined, true, undefined);
-    this.cache['rchanh'] = this.createCacheObject('tenChanh', true, item => { return item.tenChanh; });
-    this.cache['rcanhbaotonkho'] = this.createCacheObject(undefined, true, undefined);
+    this.cache['rloaihang'] = this.createCacheObject(true, item => { return item.tenLoai; });
+    this.cache['tmathang'] = this.createCacheObject(true, item => { return item.tenMatHang; });
+    this.cache['rkhohang'] = this.createCacheObject(true, item => { return item.tenKho; });
+    this.cache['rkhachhang'] = this.createCacheObject(true, item => { return item.tenKhachHang; });
+    this.cache['rnhanvien'] = this.createCacheObject(true, item => { return item.tenNhanVien; });
+    this.cache['rloaichiphi'] = this.createCacheObject(true, item => { return item.tenLoaiChiPhi; });
+    this.cache['rkhachhangchanh'] = this.createCacheObject(true, undefined);
+    this.cache['rchanh'] = this.createCacheObject(true, item => { return item.tenChanh; });
+    this.cache['rcanhbaotonkho'] = this.createCacheObject(true, undefined);
+    this.cache['rphuongtien'] = this.createCacheObject(true, item => { return item.tenPhuongTien; });
+    this.cache['rnhacungcap'] = this.createCacheObject(true, item => { return item.tenNhaCungCap; });
+    this.cache['rnguyenlieu'] = this.createCacheObject(true, undefined);
+    this.cache['rbaixe'] = this.createCacheObject(true, item => { return item.diaDiemBaiXe; });
+    this.cache['rdiadiem'] = this.createCacheObject(true, item => { return item.tenNuoc; });
+    this.cache['rloainguyenlieu'] = this.createCacheObject(true, item => { return item.tenLoai; });
   }
 
-  private createCacheObject(sortProperty, isAscending, displayTextFunc) {
+  private createCacheObject(isAscending, displayTextFunc) {
     return {
       data: [],
       isLoaded: false,
       keyProperty: this.keyProperty,
-      sortProperty: sortProperty,
+      sortProperty: this.sortProperty,
       isAscending: isAscending,
       isLoading: false,
       loadSubject: new Subject(),
-      displayTextFunc: displayTextFunc || (item => { return item[this.keyProperty]; })
+      displayTextFunc: displayTextFunc || (item => { return item[this.keyProperty] + ''; })
     }
   }
 
